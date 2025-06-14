fc01476ad4de097aff0c75ddefa226574260d8cc
hyrahmaaa <chhoiharim@gmail.com>
2025-06-14 07:42:56 +0700

    Add files via upload

---
+++ automate_Rahayu-Nur-R.py
@@ -0,0 +1,8 @@
+
+# automate_TelcoChurn_Nama-siswa.py
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+import os
