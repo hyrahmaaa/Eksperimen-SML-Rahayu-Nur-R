fb8d94e7b03a98826f1791dbf11b8afa1a20f3e9
hyrahmaaa <chhoiharim@gmail.com>
2025-06-15 13:21:24 +0700

    Update modelling_tuning.py

--- Membangun_model/modelling_tuning.py
+++ Membangun_model/modelling_tuning.py
@@ -14,11 +14,12 @@ Original file is located at
 import pandas as pd
 import numpy as np
 import mlflow
+import mlflow.sklearn
 import dagshub
+import joblib 
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 import os
 
